1ddaef8e83b1c7213af67418c43fbc3ad1db9ea8
dave <danas.jaydave@ici.edu.ph>
2025-02-07 15:07:15 +0800

    table

--- pt2_danas.php
+++ pt2_danas.php
@@ -7,10 +7,23 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div class="table">
-        <tr>
-        
-        </tr>
-    </table>>
+<table width="90%" border="1px">
+    <tbody>
+<tr>
+    <td>
+        <h1>Public Library Expansion Project</h1>
+        <h2>Cost Estimates</h2>
+    </td>
+</tr></tbody>
+    <tbody>
+    <tr>
+    <th>Expenditures</th>
+    <th>Estimated Cost</th>
+    <th>10% Increase</th>
+    <th>15% Increase</th>
+    <th>20% Increase</th>
+</tr>
+</tbody>
+  </table>
 </body>
 </html>
